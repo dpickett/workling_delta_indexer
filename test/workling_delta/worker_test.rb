66172051a22fea5a0b3227fa49324eb9e298bcbd
Dan Pickett <dpickett@enlightsolutions.com>
2009-03-07 18:52:22 -0500

    completed test harness

--- test/workling_delta/worker_test.rb
+++ test/workling_delta/worker_test.rb
@@ -1,20 +1,46 @@
 require File.join(File.dirname(__FILE__), "..", "test_helper")
 
 class WorklingDelta::WorkerTest < Test::Unit::TestCase
-    context "a worker" do
-      should "trigger a TS delta job with the index name" do
-        mock_job = mock(ThinkingSphinx::Deltas::DeltaJob).new(index_name)
-        mock_job.perform
+  context "a worker" do
+    setup do
+      @index_name = "the_delta_index"
+      @document_id = 1
 
-        WorklingDeltaIndexer.async_index
+      @job = mock(Object)
+      mock(ThinkingSphinx::Deltas::DeltaJob).new(@index_name) { @job }
+      @job.perform
+    end
+
+    context "that doesn't receive a document id" do
+      
+      should "perform a TS delta job with the index name" do
+        invoke_worker(:index_name => @index_name)
       end
 
+      should "not perform a flag as deleted job if I don't pass a document id" do
+        dont_allow(ThinkingSphinx::Deltas::FlagAsDeletedJob).new(
+         anything, anything)
+        
+        invoke_worker(:index_name => @index_name)
+      end
+
+    end
+
+    context "that receives a document id" do
       should "trigger a flag as deleted job if I pass a document id" do
+        delete_job = mock(Object)
+        mock(ThinkingSphinx::Deltas::FlagAsDeletedJob).new(
+          @index_name, @document_id) { delete_job }
+        delete_job.perform
 
-      end
-
-      should "not trigger a flag as deleted job if I don't pass a document id" do
-
+        invoke_worker(:index_name => @index_name, :document_id => @document_id)
       end
     end
+    
+  end
+
+  private
+  def invoke_worker(options = {})
+    WorklingDelta::Worker.new.index(options)
+  end
 end
