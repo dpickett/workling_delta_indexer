e140795e630935edb3f1546861d40286e1767065
Dan Pickett <dpickett@enlightsolutions.com>
2009-03-07 15:08:28 -0500

    Initial commit to workling_delta_indexer.

---
+++ test/test_helper.rb
@@ -0,0 +1,9 @@
+require 'rubygems'
+require 'test/unit'
+
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+require 'workling_delta_indexer'
+
+class Test::Unit::TestCase
+end
