324ae52b14f277b135ff401672c09cce809e1e36
Dan Pickett <dpickett@enlightsolutions.com>
2009-03-07 17:42:21 -0500

    vendored workling for test purposes

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,13 +3,18 @@ require 'test/unit'
 
 require 'shoulda'
 require 'rr'
+require 'active_support'
+require 'active_support/core_ext'
+
+RAILS_ENV = "test"
+RAILS_DEFAULT_LOGGER = "/dev/null"
 
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'vendor'))
 
-require "workling/lib/workling"
-require "workling/init"
+require "workling/lib/workling/discovery"
+require "workling/lib/workling/base"
 require 'workling_delta_indexer'
 
 class Test::Unit::TestCase
