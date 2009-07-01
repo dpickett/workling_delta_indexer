44a4f53210b1669092551b63aed2df73840742a8
Dan Pickett <dpickett@enlightsolutions.com>
2009-07-01 17:38:50 -0400

    add YARD documentation

--- lib/workling_delta/indexer.rb
+++ lib/workling_delta/indexer.rb
@@ -1,5 +1,14 @@
+# This is the class that that should be wired to your index configuration
+# @example definition in an index
+#   define_index do |index|
+#     set_property :delta => WorklingDelta::Indexer
+#   end
 module WorklingDelta
   class Indexer < ThinkingSphinx::Deltas::DefaultDelta
+    # Creates the worker that performs the delta indexing
+    # @param model the class of that triggered the delta
+    # @param instance the instance that changed (if applicable)
+    # @return [Boolean] should always return true
     def do_index(model, instance = nil)
       doc_id = instance ? instance.sphinx_document_id : nil
       WorklingDelta::Worker.async_index(:index_name => delta_index_name(model), :document_id => doc_id)
--- lib/workling_delta/worker.rb
+++ lib/workling_delta/worker.rb
@@ -1,4 +1,9 @@
+# The Worker that actually performs the delta indexing
+# This class performs the indexing asyncronously for Workling configurations
 class WorklingDelta::Worker < Workling::Base
+  #perform the index
+  #@option options [String] :index_name the name of the index to run the delta job on
+  #@option options [Integer, nil] :document_id the sphinx document id of the object that needs to be flagged as deleted
   def index(options = {})
     ThinkingSphinx::Deltas::DeltaJob.new(options[:index_name]).perform
     
